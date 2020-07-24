feef92d0078508900f70b6ab535130b9a137cb04
Sathya Sellamuthu <sathyaee27@gmail.com>
2020-07-24 13:22:50 +0530

    Externalizing log path and exception handling enhancement

--- src/main/java/com/future/works/reflect/controller/ExceptionController.java
+++ src/main/java/com/future/works/reflect/controller/ExceptionController.java
@@ -1,24 +1,22 @@
 package com.future.works.reflect.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
-public class ExceptionController {
+public class ExceptionController{
 
 	private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
 	
-	@ExceptionHandler
-	public void handleIllegalArgumentException(IllegalArgumentException excep, HttpServletResponse response) throws IOException {
+	@ExceptionHandler(value = {Exception.class})
+	public ResponseEntity<Object> handleIllegalArgumentException(Exception excep, WebRequest response) {
 		logger.debug("Exception occured {}",excep.getMessage());
-		response.sendError(HttpStatus.BAD_REQUEST.value());
+		return new ResponseEntity<>(excep.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
